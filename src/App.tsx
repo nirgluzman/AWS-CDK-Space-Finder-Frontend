b9c81e36fd7893eefe80f186c8b6af927cf648ab
nirgluzman <nir.gluzman@gmail.com>
2023-10-15 14:32:51 +0200

    add basic template for NavBar with Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+import NavBar from './components/NavBar';
+
 function App() {
+  const [userName, setUserName] = useState<string | undefined>(undefined);
+
   return (
-    <>
-      <h1>App works!</h1>
-    </>
+    <div className='wrapper'>
+      <NavBar userName={userName} />
+      <Routes>
+        <Route
+          path='/'
+          element={<div>Home</div>}
+        />
+        <Route
+          path='/login'
+          element={<div>Login page</div>}
+        />
+        <Route
+          path='/profile'
+          element={<div>Profile page</div>}
+        />
+        <Route
+          path='/createSpace'
+          element={<div>Create space page</div>}
+        />
+        <Route
+          path='/spaces'
+          element={<div>Spaces page</div>}
+        />
+      </Routes>
+    </div>
   );
 }
 
